f36efec6f289898cb86560bdb44aace09ffc0a70
xraverx <95680845+xraverx@users.noreply.github.com>
2023-04-30 16:36:52 -0500

    Update clear_package_cache.sh

--- bash/clear_package_cache.sh
+++ bash/clear_package_cache.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-#Clear package cache using pacman on Arch Linux
-sudo pacman -Scc --noconfirm
+# Clear package cache using pacman on Arch Linux
+sudo pacman -Scc
+# Use --noconfirm flag to run without confirmations
